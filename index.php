b8110cdfe5563f3603dcf0db6a341752939b5bdd
anuragmaheshwari06 <33050274+anuragmaheshwari06@users.noreply.github.com>
2019-12-05 13:28:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<frameset rows="10%,*" border="0">
+<frame src="links.php" name="home">
+<frame src="home.php" name="content">
+</frameset>
+</frameset>
+</html>
---
+++ links.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+
+	body{
+		background-color: #23232e;
+	}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #23232e;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+
+
+	</style>
+</head>
+<body>
+
+
+<div>
+
+<ul class = "nav-bar">
+  <li><a href="index.php" target="content">Home</a></li>
+  <li><a href="" target ="content">Recent</a></li>
+  
+</ul>
+</div>
+
+</body>
+</html>
